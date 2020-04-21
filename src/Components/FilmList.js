31b67c718c55c6c8474998795a4f511c622057b8
mholwill <m_holwill@hotmail.co.uk>
2020-04-21 10:15:01 +0100

    Small changes after HW revue

--- src/Components/FilmList.js
+++ src/Components/FilmList.js
@@ -4,13 +4,15 @@ import FilmItem from './FilmItem';
 export default class FilmList extends Component {
   render() {
 
-    const filmNodes = this.props.films.map(film => {
+    const filmItems = this.props.films.map(film => {
         return <FilmItem name={film.name} url={film.url} key={film.id}></FilmItem>
       })
 
     return (
       <div className="film-list">
-        {filmNodes}
+        <ul>
+          {filmItems}
+        </ul>
       </div>
     )
   }
